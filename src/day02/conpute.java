2d790382df9f2f00b25a09f674d7b78bedbd3da9
tedu <2304720722@qq.com>
2024-03-09 10:58:58 +0800

    变量的运算

--- src/day02/conpute.java
+++ src/day02/conpute.java
@@ -5,5 +5,9 @@ public class conpute {
         int a=10;
         a+=10;
         System.out.println(a);
+        //变量命名： 只能包含字母，数字，_，和$  而且数字不能开头
+        //建议小鸵峰命名法 myScore myJavaScore
+        //类名建议大坨峰 每个单词首字母大写
+
     }
 }
