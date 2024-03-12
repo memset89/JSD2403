4aa68334f28c8ac2c515cb98c34dd006c55c5026
tedu <2304720722@qq.com>
2024-03-12 14:42:31 +0800

    gameRole

--- src/day03/gameRole.java
+++ src/day03/gameRole.java
@@ -21,10 +21,10 @@ public class gameRole {
         thunder.MP=120;*/
         Random x=new Random();
         jjboy.attack=x.nextInt(1000);
-        jjboy.HP=x.nextInt(100000);
+        jjboy.HP=x.nextInt(2000000000);
         jjboy.MP=x.nextInt(500);
         thunder.attack=x.nextInt(1000);
-        thunder.HP=x.nextInt(100000);
+        thunder.HP=x.nextInt(2000000000);
         thunder.MP=x.nextInt(500);
         int x1=x.nextInt(100);
         int x2=x.nextInt(100);
@@ -39,98 +39,98 @@ public class gameRole {
         while (die){
             if (inChess){
 
-                Thread.sleep(1000);
+                //Thread.sleep(1000);
                 if (jjboy.MP>=40){
                     System.out.println("jjboy释放了技能，对thunder造成了"+jjboy.attack*1.5+"点伤害！");
                     thunder.HP-=jjboy.attack*1.5;
-                    Thread.sleep(1000);
+                    //Thread.sleep(1000);
                     jjboy.MP-=40;
-                    Thread.sleep(1000);
+                    //Thread.sleep(1000);
                     System.out.println("jjboy消耗了40点MP");
                 }
                 else {
                     thunder.HP-=jjboy.attack;
                     System.out.println("jjboy的普通攻击对thunder造成了"+jjboy.attack+"点伤害");
-                    Thread.sleep(1000);
+                    //Thread.sleep(1000);
                 }
                 System.out.println("thunder的剩余生命值"+thunder.HP);
                 if (thunder.HP<=0){
                     System.out.println("thunder阵亡");
-                    Thread.sleep(1000);
+                    //Thread.sleep(1000);
                     break;
                 }
-                Thread.sleep(1000);
+                //Thread.sleep(1000);
                 if (thunder.MP>=40){
                     System.out.println("thunder释放了技能，对jjboy造成了"+thunder.attack*1.5+"点伤害！");
                     jjboy.HP-= thunder.attack*1.5;
-                    Thread.sleep(1000);
+                    //Thread.sleep(1000);
                     System.out.println("thunder消耗了40点MP");
                     thunder.MP-=40;
-                    Thread.sleep(1000);
+                    //Thread.sleep(1000);
                 }
                 else {
-                    Thread.sleep(1000);
+                    //Thread.sleep(1000);
                     System.out.println("thunder的普通攻击对jjboy造成了"+thunder.attack+"点伤害");
                     jjboy.HP-= thunder.attack;
                 }
-                Thread.sleep(1000);
+                //Thread.sleep(1000);
                 System.out.println("jjboy的剩余生命值"+jjboy.HP);
                 if (jjboy.HP<=0){
-                    Thread.sleep(1000);
+                    //Thread.sleep(1000);
                     System.out.println("jjboy阵亡");
                     break;
                 }
-                Thread.sleep(1000);
+                //Thread.sleep(1000);
                 jjboy.MP+=10;thunder.MP+=10;
                 System.out.println("jjboy和thunder回复了10点MP");
-                Thread.sleep(1000);
+                //Thread.sleep(1000);
             }
             else {
 
                 if (thunder.MP>=40){
                     System.out.println("thunder释放了技能，对jjboy造成了"+thunder.attack*1.5+"点伤害！");
                     jjboy.HP-= thunder.attack*1.5;
-                    Thread.sleep(1000);
+                    //Thread.sleep(1000);
                     System.out.println("thunder消耗了40点MP");
                     thunder.MP-=40;
-                    Thread.sleep(1000);
+                    //Thread.sleep(1000);
                 }
                 else {
-                    Thread.sleep(1000);
+                    //Thread.sleep(1000);
                     System.out.println("thunder的普通攻击对jjboy造成了"+thunder.attack+"点伤害");
                     jjboy.HP-= thunder.attack;
                 }
-                Thread.sleep(1000);
+                //Thread.sleep(1000);
                 System.out.println("jjboy的剩余生命值"+jjboy.HP);
                 if (jjboy.HP<=0){
-                    Thread.sleep(1000);
+                    //Thread.sleep(1000);
                     System.out.println("jjboy阵亡");
                     break;
                 }
-                Thread.sleep(1000);
+                //Thread.sleep(1000);
                 if (jjboy.MP>=40){
                     System.out.println("jjboy释放了技能，对thunder造成了"+jjboy.attack*1.5+"点伤害！");
                     thunder.HP-=jjboy.attack*1.5;
-                    Thread.sleep(1000);
+                    //Thread.sleep(1000);
                     jjboy.MP-=40;
-                    Thread.sleep(1000);
+                    //Thread.sleep(1000);
                     System.out.println("jjboy消耗了40点MP");
                 }
                 else {
                     thunder.HP-=jjboy.attack;
                     System.out.println("jjboy的普通攻击对thunder造成了"+jjboy.attack+"点伤害");
-                    Thread.sleep(1000);
+                    //Thread.sleep(1000);
                 }
                 System.out.println("thunder的剩余生命值"+thunder.HP);
                 if (thunder.HP<=0){
                     System.out.println("thunder阵亡");
-                    Thread.sleep(1000);
+                    //Thread.sleep(1000);
                     break;
                 }
-                Thread.sleep(1000);
+                //Thread.sleep(1000);
                 jjboy.MP+=10;thunder.MP+=10;
                 System.out.println("jjboy和thunder回复了10点MP");
-                Thread.sleep(1000);
+                //Thread.sleep(1000);
             }
         }
         if (jjboy.HP>0){
